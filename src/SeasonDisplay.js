bf1f712dd24d93af8f1e1537bdbe72d934960732
Dharmik6017 <borichadharmik@gmail.com>
2020-11-06 17:55:18 +0530

    fixes

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SeasonDisplay = () => {
+  return <div>Hello Therer</div>;
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
-const App = () => {
-  return <div>Hello Therere</div>;
-};
+// const App = () => {
+
+//   return (
+//     <div className="ui container comments">
+//       <ApprovalCard>
+//         <CommentDetail
+//           author="Alex"
+//           date="Today at 6:00PM"
+//           text="My first blog!!"
+//         />
+//       </ApprovalCard>
+//     </div>
+//   );
+// };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log("error", err)
+    );
+  }
+
+  render() {
+    return <div>Hello Therree {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
